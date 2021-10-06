077a63ad571e02d715ba2c5a33f5a2d2232e8974
aledosreis <alessandrodreis@outlook.com>
2021-10-05 22:21:36 -0300

    criado tela de lições

--- app/src/main/java/com/tcc/colcheius/MainActivity.kt
+++ app/src/main/java/com/tcc/colcheius/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lessonsFragment = LessonsFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container,lessonsFragment)
+            commit()
+        }
     }
 }
