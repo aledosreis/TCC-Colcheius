8c831f711277d7314575a4bb2d54dd28d363b6d8
aledosreis <alessandrodreis@outlook.com>
2021-10-05 21:50:01 -0300

    inicio da tela principal e menu da bottomnavigation com icones

--- app/src/main/java/com/tcc/colcheius/LoginActivity.kt
+++ app/src/main/java/com/tcc/colcheius/LoginActivity.kt
@@ -4,36 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.core.content.IntentCompat
-
-
-
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val etEmail = findViewById<EditText>(R.id.et_email)
-        val etPassword = findViewById<EditText>(R.id.et_password)
         val btLogin = findViewById<Button>(R.id.bt_login)
 
         btLogin.setOnClickListener {
-            val email = etEmail.text.toString()
-            val password = etPassword.text.toString()
-
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                doLogin(email, password)
-            } else {
-                Toast.makeText(this, "Obrigatório preencher todos os campos", Toast.LENGTH_SHORT).show()
-            }
+            doLogin()
         }
     }
 
-    private fun doLogin(email: String, password: String) {
-        //verificar usuário e senha
+    private fun doLogin() {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
