7338378c267a0f97832b6360762eeef81d08babf
aledosreis <alessandrodreis@outlook.com>
2021-10-08 20:14:31 -0300

    verificando se usuario está logado ao abrir app e realizando logout do usuario

--- app/src/main/java/com/tcc/colcheius/ProfileFragment.kt
+++ app/src/main/java/com/tcc/colcheius/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package com.tcc.colcheius
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +37,23 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val btLogout : TextView = view.findViewById(R.id.logout)
+        btLogout.setOnClickListener {
+            logoutUser()
+        }
+
+        return view
+    }
+
+    /**
+     * Método responsável por realizar o logout do usuário.
+     */
+    private fun logoutUser() {
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(requireContext(), StartActivity::class.java))
+        activity?.finish()
     }
 
     companion object {
