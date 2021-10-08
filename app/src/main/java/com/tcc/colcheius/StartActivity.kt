84c3ee9cf5d3ea1c57eb093303714dc2415ecac3
Alessandro Reis <alessandrodreis23@gmail.com>
2021-10-08 20:14:31 -0300

    verificando se usuario está logado ao abrir app e realizando logout do usuario

--- app/src/main/java/com/tcc/colcheius/StartActivity.kt
+++ app/src/main/java/com/tcc/colcheius/StartActivity.kt
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        verifyAuthentication()
+
         setContentView(R.layout.activity_start)
 
         val btLogin = findViewById<Button>(R.id.bt_login)
@@ -20,6 +24,14 @@ class StartActivity : AppCompatActivity() {
 
         tvNewAccount.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    private fun verifyAuthentication() {
+        if (FirebaseAuth.getInstance().uid != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
         }
     }
 }
