cd0ad103fcc4eed44303ef0af3989ef1be0f4d63
Alessandro Reis <alessandrodreis23@gmail.com>
2021-10-09 14:34:50 -0300

    adicionando listas estaticas ao ranking e a tela de módulos

--- app/src/main/java/com/tcc/colcheius/LessonsFragment.kt
+++ app/src/main/java/com/tcc/colcheius/LessonsFragment.kt
@@ -5,51 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LessonsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LessonsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val modules = mutableListOf(
+        "Módulo 1",
+        "Módulo 2",
+        "Módulo 3",
+        "Módulo 4",
+        "Módulo 5",
+        "Módulo 6",
+        "Módulo 7",
+        "Módulo 8",
+        "Módulo 9",
+        "Módulo 10"
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_lessons, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.lesson_list)
+
+        val adapter: GroupAdapter<ViewHolder> = GroupAdapter<ViewHolder>()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this.requireContext())
+
+        for (module in modules) {
+            adapter.add(ModuleItem(module))
         }
+
+        return view
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lessons, container, false)
-    }
+    private inner class ModuleItem(var moduleName: String) : Item<ViewHolder>() {
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+            val tvModuleName: TextView = viewHolder.itemView.findViewById(R.id.module_name)
+            val imageGoOn : ImageView = viewHolder.itemView.findViewById(R.id.go_on_btn)
+            tvModuleName.text = moduleName
+            if (moduleName != "Módulo 1") {
+                imageGoOn.setImageResource(R.drawable.ic_lock)
+            }
+        }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LessonsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                LessonsFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        override fun getLayout(): Int {
+            return R.layout.item_list_module
+        }
+
     }
 }
--- app/src/main/java/com/tcc/colcheius/RankingFragment.kt
+++ app/src/main/java/com/tcc/colcheius/RankingFragment.kt
@@ -5,51 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RankingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RankingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val mapUserScore = mutableMapOf("Alessandro" to 100, "Caynã" to 95, "Outro cara" to 50)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ranking, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_ranking, container, false)
+        val recyclerView : RecyclerView = view.findViewById(R.id.ranking_list)
+        val adapter : GroupAdapter<ViewHolder> = GroupAdapter<ViewHolder>()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this.requireContext())
+
+        for (item in mapUserScore) {
+            adapter.add(RankingItem(item.key, item.value))
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RankingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                RankingFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    private inner class RankingItem(val username : String, val score : Int) : Item<ViewHolder>() {
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+            val tvUsername : TextView = viewHolder.itemView.findViewById(R.id.user_name)
+            val tvScore : TextView = viewHolder.itemView.findViewById(R.id.user_score)
+
+            tvUsername.text = username
+            tvScore.text = score.toString()
+        }
+
+        override fun getLayout(): Int {
+            return R.layout.item_list_ranking
+        }
+
     }
 }
