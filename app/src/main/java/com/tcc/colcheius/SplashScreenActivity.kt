4196afa3dd03fbc8066f8b059bda1dd59c0a6b25
Alessandro Reis <alessandrodreis23@gmail.com>
2021-10-08 22:16:47 -0300

    criado a splash screen - closes #14

---
+++ app/src/main/java/com/tcc/colcheius/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.tcc.colcheius
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (FirebaseAuth.getInstance().uid == null) {
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        finish()
+
+    }
+}
