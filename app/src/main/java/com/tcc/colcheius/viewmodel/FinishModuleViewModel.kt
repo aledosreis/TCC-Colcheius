b2e6e754b71c063ff1a4642bd339bd9899d03afd
Alessandro Reis <alessandrodreis23@gmail.com>
2021-10-21 20:18:57 -0300

    somando pontos e desbloqueando módulo seguinte

--- app/src/main/java/com/tcc/colcheius/viewmodel/FinishModuleViewModel.kt
+++ app/src/main/java/com/tcc/colcheius/viewmodel/FinishModuleViewModel.kt
@@ -3,6 +3,9 @@ package com.tcc.colcheius.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 import com.tcc.colcheius.model.Answer
 
 class FinishModuleViewModel(
@@ -29,5 +32,22 @@ class FinishModuleViewModel(
         _percent.value = percentCorrect
         _score.value = finalScore
         _answersArray.value = answers
+
+        updateUserTotalScore()
+        unlockNextModule()
+    }
+
+    fun updateUserTotalScore() {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        FirebaseFirestore.getInstance().collection("users").document(userId)
+            .update("totalScore", FieldValue.increment(score.value?.toLong()!!))
+    }
+
+    fun unlockNextModule() {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        if (percent.value!! >= 60) {
+            FirebaseFirestore.getInstance().collection("users").document(userId)
+                .update("unlockedModules", FieldValue.arrayUnion("Módulo ${module.value?.plus(1)}"))
+        }
     }
 }
