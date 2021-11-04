3e63daee75c909242a007bee32608ed72b990093
aledosreis <alessandrodreis@outlook.com>
2021-11-03 21:18:12 -0300

    teoria do modulo 2

---
+++ app/src/main/java/com/tcc/colcheius/view/FigurasThreeFragment.kt
@@ -0,0 +1,30 @@
+package com.tcc.colcheius.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.tcc.colcheius.R
+
+class FigurasThreeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val gravesAgudosFragment = GravesAgudosFragment()
+        val view = inflater.inflate(R.layout.fragment_figuras_three, container, false)
+
+        val btnContinue : Button = view.findViewById(R.id.btnContinue)
+        btnContinue.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()?.apply {
+                replace(R.id.container_lesson, gravesAgudosFragment)
+                commit()
+            }
+        }
+
+        return view
+    }
+}
