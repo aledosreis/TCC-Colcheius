1b2f65873014b39a55aff4b6515e20977351cf83
aledosreis <alessandrodreis@outlook.com>
2021-11-06 15:14:33 -0300

    adicionado aviso antes de iniciar as questões

--- app/src/main/java/com/tcc/colcheius/view/modulo1/SongPropertyTheoryFragment.kt
+++ app/src/main/java/com/tcc/colcheius/view/modulo1/SongPropertyTheoryFragment.kt
@@ -1,16 +1,22 @@
 package com.tcc.colcheius.view.modulo1
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.tcc.colcheius.R
 import com.tcc.colcheius.view.QuestionsActivity
 
 class SongPropertyTheoryFragment : Fragment() {
+
+    private var module : Int? = 0
+    private lateinit var whatIsMusicTheoryFragment: WhatIsMusicTheoryFragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,18 +24,34 @@ class SongPropertyTheoryFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_song_property, container, false)
 
-        val module = activity?.intent?.extras?.getInt("module")
+        module = activity?.intent?.extras?.getInt("module")
+        whatIsMusicTheoryFragment = WhatIsMusicTheoryFragment()
 
         val btnContinue : Button = view.findViewById(R.id.btnContinue)
         btnContinue.setOnClickListener {
-//            Toast.makeText(requireContext(), module.toString(), Toast.LENGTH_SHORT).show()
-            val intent = Intent(requireContext(), QuestionsActivity::class.java)
-            intent.putExtra("module", module)
-            startActivity(intent)
-            activity?.finish()
+            showDialog()
         }
 
         return view
+    }
+
+    private fun showDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("ESTÁ PRONTO PARA PROSSEGUIR?")
+            .setMessage("A seguir serão apresentados exercícios para fixar o conteúdo abordado.\nCaso deseje voltar, pressione \"Revisar\" abaixo.")
+            .setCancelable(false)
+            .setPositiveButton("PROSSEGUIR", DialogInterface.OnClickListener { _, _ ->
+                val intent = Intent(requireContext(), QuestionsActivity::class.java)
+                intent.putExtra("module", module)
+                startActivity(intent)
+                activity?.finish()
+            })
+            .setNegativeButton("REVISAR", DialogInterface.OnClickListener { _, _ ->
+                activity?.supportFragmentManager?.beginTransaction()?.apply {
+                    replace(R.id.container_lesson,whatIsMusicTheoryFragment)
+                    commit()
+                }
+            }).show()
     }
 
 }
